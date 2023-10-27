dc359194d7f7e064cb1e30db9ccf30c292504281
jjung1995 <149158260+jjung1995@users.noreply.github.com>
2023-10-27 19:33:59 +0900

    Update run_models.py

--- run_models.py
+++ run_models.py
@@ -7,15 +7,8 @@ import bisect
 import random
 
 from san_models.DMRModel import DMRModel 
-from san_models.DMRModel1 import DMRModel1 # simple DMR (no comparison) 
-from san_models.DMRModel2 import DMRModel2 # simple DMR + torch.eq
-from san_models.DMRModel3 import DMRModel3 # simple DMR + dummy if statement
-from san_models.DMRModel4 import DMRModel4 # simple DMR + torch.eq + dummy if statement
 from san_models.ConcatModel import ConcatModel 
 from san_models.SanityCheckModel import SanityCheckModel # Sanity-Check but only spatial redundancy
-from san_models.SanityCheckModel1 import SanityCheckModel1 # Sanity-Check without error checking
-from san_models.SanityCheckModel2 import SanityCheckModel2 # Sanity-Check + simplified error checking
-from san_models.SanityCheckModel3 import SanityCheckModel3 # Sanity-Check + checksum calculation + dummy if statement
 from san_models.MaintainingSanityModel import MaintainingSanityModel 
 from san_models.SimplifiedMaintainingSanityModel import SimplifiedMaintainingSanityModel # Maintaining Sanity but spatial redundancy only
 
@@ -25,37 +18,12 @@ alex_model.eval()
 
 dmr_model = DMRModel(alex_model)
 dmr_model.eval()
-
-dmr_model1 = DMRModel1(alex_model)
-dmr_model1.eval()
-
-dmr_model2 = DMRModel2(alex_model)
-dmr_model2.eval()
-
-dmr_model3 = DMRModel3(alex_model)
-dmr_model3.eval()
-
-dmr_model4 = DMRModel4(alex_model)
-dmr_model4.eval()
-
 concat_model = ConcatModel(alex_model)
 concat_model.eval()
-
 sc_model = SanityCheckModel(alex_model)
 sc_model.eval()
-
-sc_model1 = SanityCheckModel1(alex_model)
-sc_model1.eval()
-
-sc_model2 = SanityCheckModel2(alex_model)
-sc_model2.eval()
-
-sc_model3 = SanityCheckModel3(alex_model)
-sc_model3.eval()
-
 ms_model = MaintainingSanityModel(alex_model)
 ms_model.eval()
-
 sms_model = SimplifiedMaintainingSanityModel(alex_model)
 sms_model.eval()
 
@@ -65,9 +33,8 @@ w = 224
 c = 3
 
 device = torch.device("cuda")
-#test_models = [alex_model, alex_model, dmr_model, dmr_model1, dmr_model2, dmr_model3, dmr_model4]#, concat_model, sc_model, sms_model, ms_model]
-#test_models = [alex_model, alex_model, sc_model, sc_model1, sc_model2, sc_model3]
-test_models = [sc_model]
+test_models = [alex_model, alex_model, dmr_model, concat_model, sc_model, sms_model, ms_model] # must run a dummy model first
+
 for m in test_models:
     m.to(device)
     runtime = 0
@@ -83,41 +50,3 @@ for m in test_models:
         runtime_cpu += end_time_cpu - start_time_cpu
     print(m.__class__.__name__, runtime_cpu)
     
-    
-"""
-class QuantizedModel(torch.nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model_fp32 = model
-        self.quant = torch.quantization.QuantStub()
-        self.dequant = torch.quantization.DeQuantStub()
-        
-    def forward(self, x):
-        x = self.quant(x)
-        x = self.model_fp32(x)
-        x = self.dequant(x)
-        return x
-
-q_model = QuantizedModel(dmr_model)
-
-backend = "qnnpack"
-q_model.qconfig = torch.quantization.get_default_qconfig(backend)
-torch.backends.quantized.engine = backend
-model_static_quantized = torch.quantization.prepare(q_model, inplace=False)
-model_static_quantized = torch.quantization.convert(model_static_quantized, inplace=False)
-model_static_quantized.eval()
-
-image = torch.rand((1, 3, 224, 224))
-with torch.no_grad():
-    output = model_static_quantized(image)
-
-    
-class SanityCheck(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x, y, t):
-        torch.eq(x,y)
-        #    print(t)
-        #if not torch.sum(torch.abs(torch.sub(x,y))) < t:
-        #    print("Error Found! Layer: ", layer)
-        return None
-"""
