a3529fc59ffce9c19f1cd829e6962514e2a2e99a
jjung1995 <149158260+jjung1995@users.noreply.github.com>
2023-11-16 01:47:01 +0900

    Update and rename FineTMRModel.py to InputTMRFineModel.py

--- sanity_models/InputTMRFineModel.py
+++ sanity_models/InputTMRFineModel.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-class FineTMRModel(nn.Module):
+class InputTMRFineModel(nn.Module):
     def __init__(self, model):
-        super(FineTMRModel,self).__init__()
+        super(InputTMRFineModel,self).__init__()
         self.features = model.features
         self.avgpool = model.avgpool
         self.classifier = model.classifier
@@ -15,17 +15,19 @@ class FineTMRModel(nn.Module):
         for layer in self.features:
             x = layer(x)
             if torch.equal(x[0],x[1]): #torch.max(torch.abs(torch.sub(x[0],x[1]))) < self.threshold:
-                x[2] = x[0].clone().detach()
+                if not torch.equal(x[0],x[2]):
+                    x[2] = x[0].clone().detach()
             elif torch.equal(x[1],x[2]): #torch.max(torch.abs(torch.sub(x[1],x[2]))) < self.threshold:
                 x[0] = x[1].clone().detach()
             elif torch.equal(x[0],x[2]): #torch.max(torch.abs(torch.sub(x[0],x[2]))) < self.threshold:
                 x[1] = x[2].clone().detach()
             else:
                 print("Unrecoverable Error")
-                return x
+                return None
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         if torch.equal(x[0],x[1]): #torch.max(torch.abs(torch.sub(x[0],x[1]))) < self.threshold:
+            if not torch.equal(x[0],x[2]):
                 x[2] = x[0].clone().detach()
         elif torch.equal(x[1],x[2]): #torch.max(torch.abs(torch.sub(x[1],x[2]))) < self.threshold:
             x[0] = x[1].clone().detach()
@@ -33,16 +35,17 @@ class FineTMRModel(nn.Module):
             x[1] = x[2].clone().detach()
         else:
             print("Unrecoverable Error")
-            return x
+            return None
         for layer in self.classifier:
             x = layer(x)
             if torch.equal(x[0],x[1]): #torch.max(torch.abs(torch.sub(x[0],x[1]))) < self.threshold:
-                x[2] = x[0].clone().detach()
+                if not torch.equal(x[0],x[2]):
+                    x[2] = x[0].clone().detach()
             elif torch.equal(x[1],x[2]): #torch.max(torch.abs(torch.sub(x[1],x[2]))) < self.threshold:
                 x[0] = x[1].clone().detach()
             elif torch.equal(x[0],x[2]): #torch.max(torch.abs(torch.sub(x[0],x[2]))) < self.threshold:
                 x[1] = x[2].clone().detach()
             else:
                 print("Unrecoverable Error")
-                return x
+                return None
         return x
